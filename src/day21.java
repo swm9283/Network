44f66d44a0c6738c52c56a793deeb3cc821034dd
uminsong1512 <uminsong1512@gmail.com>
2023-02-13 11:21:19 +0900

    Netowork 01.1

--- src/day21.java
+++ src/day21.java
@@ -6,7 +6,7 @@ public class day21
 {
     public static void main(String[] args) {
         try {
-            InetAddress addr = Inet4Address.getByName("www.inha.ac.kr");
+            InetAddress addr = Inet4Address.getByName("www.inha.ac.kr");  // 어떻게 new가 없이 객체 생성? 내부적으로 객체를 생성하고 있을 수 있다.
 //            InetAddress addr = Inet4Address.getByName("www.kiminha.ac.kr");
             System.out.println(addr);
             System.out.println(addr.getCanonicalHostName());
