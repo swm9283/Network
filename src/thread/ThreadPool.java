c88a8c38994bb0f5d81fdbd513137021fd6a693b
uminsong1512 <uminsong1512@gmail.com>
2023-02-15 00:16:33 +0900

    ThreadPool 04 작업 완료 통보

--- src/thread/ThreadPool.java
+++ src/thread/ThreadPool.java
@@ -5,35 +5,53 @@ import com.sun.jdi.ThreadReference;
 import java.util.concurrent.*;
 
 
-public class ThreadPool {
-    public static void main(String[] args) throws InterruptedException {
+public class ThreadPool
+{
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors());
+//
+//        System.out.println("작업 처리 요청");
+//        Runnable runnable = () -> {
+//            int total = 0;
+//            for(int i = 0 ; i <= 10 ; ++i)
+//            {
+//                total = total + i;
+//            }
+//            System.out.printf("[처리 결과] : %d\n",total);
+//        };
+//        Future<?> future = executorService.submit(runnable);
+//
+//        try{
+//            future.get();
+//            System.out.println("작업 처리 완료");
+//        } catch (ExecutionException e) {
+//            System.out.printf("실행 예외 발생함 : %s",e.getMessage());
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        executorService.shutdown();
 
-        //newFixedThreadPool(int nThreads)
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-
-        // 작업 처리 요청이란 ExecutorService의 작업 큐애 Runnable 또는 Collable 객체를 넣는 행위를 말한다.
-        //submit(Runnable task) submit(Runnable task,V result) submit(Collable task)
-        //Future 객체 리턴
-        //Runnable 또는 Collable 을 작업 큐에 저장
-        // return 된 Future를 통해 작업 처리 결과를 알 수 있음
-        // 예외가 발생하더라도 스레드는 종료되지 않고 다른 작업에 재사용될 수 있음.
-        // 가급적 이 메소드를 사용하는 것을 추천
-
-        for(int i = 0 ; i <10 ; ++i)
-        {
-            Runnable runnable = () ->
+        System.out.println("작업 처리 요청");
+        Callable<Integer> task = ()->{
+            int total = 0;
+            for(int k = 0 ; k <= 10 ; ++k)
             {
-                ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executorService;
-                int poolSize = threadPoolExecutor.getPoolSize();
-                String threadName = Thread.currentThread().getName();
-                System.out.printf("[총 스레드 개수 : %d 작업 스레드 이름 : %s]\n",poolSize,threadName);
-                int value = Integer.parseInt("삼");
-            };
-            executorService.submit(runnable);
-            Thread.sleep(100);
+                total = total + k;
+            }
+            return total;
+        };
+        Future<Integer> future = executorService.submit(task);
 
+        try{
+            int total = future.get();
+            System.out.printf("{처리 결과} : %d\n",total);
+            System.out.println("작업 처리 완료");
+        } catch (ExecutionException e) {
+            System.out.println("실행 예외 발생함." + e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
         executorService.shutdown();
-        // 예외가 발생하더라도 기존 스레드를 재활용하여 작업을 처리하는 것을 알 수 있다.
     }
 }
