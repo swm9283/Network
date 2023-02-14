69c96e7c4ac1700354ed4c5f82bbedfa6ffb5f4c
uminsong1512 <uminsong1512@gmail.com>
2023-02-14 18:34:25 +0900

    Thread Misson 02

--- src/thread/threadMission.java
+++ src/thread/threadMission.java
@@ -46,12 +46,54 @@ class RunnableWorker implements Runnable
 public class threadMission {
     public static void main(String[] args) {
 
-        Worker worker = new Worker();
-        worker.start();
+//        Thread a = new Thread(() ->{
+//            for(int i = 0 ; i <= 5 ; ++i)
+//            {
+//                System.out.println(i);
+//            }
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        } );
+//        a.start();
+        //Runnable 인터페이스 구현 함수형 인터페이스의 구현 객체이다.
+        Thread thread = new Thread(new RunnableWorker());
+//        thread.start();
 
+        //lambda로 스레드 생성.
+        Thread thread1 = new Thread(() ->{
+            for(int i = 0 ; i<=5 ; ++i)
+            {
+                System.out.println(i);
+                try{
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread1.start();
+
+//        int alphabet = 'a';
+//        while (true) {
+//            if (thread.getState() == Thread.State.TERMINATED) {
+//                System.out.println("Thread가 종료되었습니다.");
+//                break;
+//            }
+//            try
+//            {
+//                Thread.sleep(5000);
+//                System.out.println((char) alphabet);
+//                alphabet = alphabet + 1;
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
         int alphabet = 'a';
         while (true) {
-            if (worker.getState() == Thread.State.TERMINATED) {
+            if (thread1.getState() == Thread.State.TERMINATED) {
                 System.out.println("Thread가 종료되었습니다.");
                 break;
             }
@@ -64,6 +106,7 @@ public class threadMission {
                 throw new RuntimeException(e);
             }
         }
+
     }
 }
 
