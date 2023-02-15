b6419591f7b0fb533a0d127478c01c1d3d719fff
uminsong1512 <uminsong1512@gmail.com>
2023-02-15 15:28:10 +0900

    Static method로 객체 생성 생성자에 private을 사용했을 경우

--- src/TestJava.java
+++ src/TestJava.java
@@ -1,21 +1,18 @@
-//StringBuilder Vs String
-//string append 승리
+class One{
+    private One()
+    {
 
-public class TestJava {
+    }
+    public static One produceOneInstance()
+    {
+        return new One();
+    }
+
+}
+
+public class TestJava
+{
     public static void main(String[] args) {
-        String s = new String("hi");
-        long start = System.nanoTime();
-        for(int i = 0 ; i<=100000 ; ++i)
-            s = s + "!";
-        long end = System.nanoTime();
-        System.out.println(end - start);
-
-        StringBuilder sb = new StringBuilder("hi");
-        long start1 = System.nanoTime();
-        for(int i = 0 ; i<=100000 ; ++i)
-            sb = sb.append("!");
-        long end1 = System.nanoTime();
-        System.out.println(end1 - start1);
-
+        One o = One.produceOneInstance();
     }
 }
